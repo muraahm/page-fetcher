f747c432887a01e616f366e949ca89ce1f2b6df3
muraahm <ahmedmuraahm121@gmail.com>
2019-08-01 06:19:51 +0000

    page downloader

---
+++ fetcher.js
@@ -0,0 +1,40 @@
+const request = require('request');
+const fs = require("fs");
+const args = process.argv.slice(2);//get arguments as an array
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const fetcher = function (URL, localFile) {
+  request(URL, (error, response, body) => {
+    if (response.statusCode === 200) {
+
+      if (fs.existsSync(localFile)) {
+        rl.question(localFile + ' already exist. replace? ', (answer) => {
+          if (answer === 'y') {
+            fs.writeFile(localFile, body, (err) => {
+              if (err) throw err;
+              console.log('Downloaded and saved ' + body.length + ' bytes to ' + localFile);
+            });
+          } else {
+            console.log('tahnks!');
+          }
+          rl.close();
+        });
+      } else {
+        fs.writeFile(localFile, body, (err) => {
+          if (err) throw err;
+          console.log('Downloaded and saved ' + body.length + ' bytes to ' + localFile);
+        });
+        rl.close();
+      }
+    } else {
+      console.log('Error web page not found');
+    }
+  });
+};
+
+fetcher(args[0], args[1]);
